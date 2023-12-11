8ae515b7f909cef879b3ba008eb8855c9ae60b3a
Алексей Алдокимов <141712991+rocketman2014@users.noreply.github.com>
2023-12-11 20:34:51 +0300

    All work

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,19 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private final Timer timer = new Timer();
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
 
     public void whatAnimalSay() {
